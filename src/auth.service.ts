e72faf3cc974f5775947a3d44623849efc20614a
rithik56 <rithik19901998@gmail.com>
2023-08-30 12:28:15 +0530

    added env file

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBEktUamz7dJRr4EvoGK4cjmdhVrpJnSwE",
-    authDomain: "fuelbuddy-assignment-e0325.firebaseapp.com",
-    projectId: "fuelbuddy-assignment-e0325",
-    storageBucket: "fuelbuddy-assignment-e0325.appspot.com",
-    messagingSenderId: "386526887676",
-    appId: "1:386526887676:web:8187838fda3181b3546179",
-    measurementId: "G-EPY7RMNNJ1"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
